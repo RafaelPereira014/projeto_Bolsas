0cbcb5009412da753872df0415a0903a0076a7bc
RafaelPereira014 <rafaelbotelhopereira@ua.pt>
2024-10-31 10:39:18 -0100

    random fake users generator

--- db_operations/user/db_user.py
+++ db_operations/user/db_user.py
@@ -12,31 +12,23 @@ def get_user_info(user_ids):
     try:
         placeholders = ', '.join(['%s'] * len(user_ids))
         query = f"""
-        SELECT u.id AS candidato_id, 
-               u.nome, 
-               u.avaliacao_curricular, 
-               u.prova_de_conhecimentos, 
-               u.nota_final, 
-               GROUP_CONCAT(ub.contrato_id) AS contrato_ids,
-               u.estado
+        SELECT DISTINCT u.id AS candidato_id, u.nome, u.avaliacao_curricular, u.prova_de_conhecimentos, u.nota_final, ub.contrato_id, u.estado
         FROM Users u
         JOIN userbolsas ub ON u.id = ub.user_id
-        WHERE u.id IN ({placeholders}) 
-        GROUP BY u.id, u.nome, u.avaliacao_curricular, u.prova_de_conhecimentos, u.nota_final, u.estado
-        ORDER BY u.nota_final DESC
+        WHERE u.id IN ({placeholders}) ORDER BY u.nota_final DESC
         """
         cursor.execute(query, user_ids)
         results = cursor.fetchall()
 
-        # Return results as a list of dictionaries
+        # Return results as a list of dictionaries, including contrato_id
         return [{
             "id": row[0], 
             "nome": row[1], 
             "avaliacao_curricular": row[2], 
             "prova_de_conhecimentos": row[3], 
             "nota_final": row[4],
-            "contrato_ids": row[5],  # Updated to store aggregated contrato_ids
-            "estado": row[6]
+            "contrato_id": row[5],
+            "estado": row[6]# Include contrato_id here
         } for row in results]
 
     except Exception as e:
